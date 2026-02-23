e9abd9cc599f1ebee08fc70732932fc035197b4f
truongthuan <truongthuan.r@gmail.com>
2026-02-23 16:09:57 +0700

    update homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import './App.css'
 import HomeChoice from './pages/HomeChoice'
 import Home from './pages/Home'
 import Lobby from './pages/Lobby'
 
 function App() {
+  const location = useLocation()
+
+  useEffect(() => {
+    console.log('[route]', location.pathname, '| window', window.location.pathname)
+  }, [location.pathname])
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/create" element={<Home />} />
       <Route path="/home" element={<HomeChoice />} />
       <Route path="/lobby" element={<Lobby />} />
       <Route path="*" element={<Navigate to="/" replace />} />
