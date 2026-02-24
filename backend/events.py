f589b33976cf4d9166f26c6cc0d7d36b434a6aa9
truongthuan <truongthuan.r@gmail.com>
2026-02-24 17:40:35 +0700

    update ws

--- backend/events.py
+++ backend/events.py
@@ -4,6 +4,7 @@ from enum import Enum
 class EventType(str, Enum):
     room_join = "room:join"
     room_leave = "room:leave"
+    room_sync = "room:sync"
     room_update = "room:update"
     game_start = "game:start"
     turn_play = "turn:play"
